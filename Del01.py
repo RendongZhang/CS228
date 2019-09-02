09e83c3db8057ff60d237a4d4253ae34e24d8fe8
rendong <rzhang6@uvm.edu>
2019-09-02 13:18:24 -0400

    del01 end

--- Del01.py
+++ Del01.py
@@ -6,9 +6,7 @@ from Leap import Finger
 from Leap import Bone
 import random
 from pygameWindow import PYGAME_WINDOW
-
-
-
+import constants
 
 x = 450
 y = 300
@@ -28,36 +26,66 @@ def Perturb_Circle_Position():
         y = y + 1
 
 def Handle_Frame(frame):
-    global x,y
+    global x,y,xMin,xMax,yMin,yMax
     hand = frame.hands[0]
-    print(hand)
+
 
     fingers = hand.fingers
 
     indexFingerList = hand.fingers.finger_type(Finger.TYPE_INDEX)
     indexFinger = indexFingerList[0]
-    print(indexFinger)
+
     distalPhalanx = indexFinger.bone(Bone.TYPE_DISTAL)
     tip = distalPhalanx.next_joint
     x = int(tip[0])
     y = int(tip[1])
-    
-    print(type(tip))
-    print(tip)
-    print(x)
-    print(y)
+
+
+    if (x < xMin):
+        xMin = x
+    if (x > xMax):
+        xMax = x
+    if (y < yMin):
+        yMin = y
+    if (y > yMax):
+        yMax = y
+
+
+def ScalePygameValue(oldValue,oldMin,oldMax,newMin,newMax,axis):
+    oldRange = (oldMax - oldMin)
+    if oldRange == 0:
+        newValue = newMin
+
+    else:
+        newRange = (newMax - newMin)
+        newValue = (((oldValue - oldMin) * newRange) / oldRange) + newMin
+    if axis == "x":
+        return int(newValue)
+    else:
+        return (newMax-int(newValue))
+
 
 controller = Leap.Controller()
+xMin = 1000.0
+xMax = -1000.0
+yMin = 1000.0
+yMax = -1000.0
 while True:
     pygameWindow.Prepare()
     frame = controller.frame()
     handlist = frame.hands
-
+    pygameX = constants.pygameWindowWidth/2
+    pygameY = constants.pygameWindowDepth/2
     for hand in handlist:
         if (len(str(hand)) > 0):
             Handle_Frame(frame)
+            pygameX = ScalePygameValue(x, xMin, xMax, 0, constants.pygameWindowWidth,"x")
+            pygameY = ScalePygameValue(y, yMin, yMax, 0, constants.pygameWindowDepth,"y")
 
-    pygameWindow.Draw_Black_Circle(x,y)
+            #print(pygameX,pygameY)
+
+    pygameWindow.Draw_Black_Circle(pygameX,pygameY)
+
     Perturb_Circle_Position()
     pygameWindow.Reveal()
 
