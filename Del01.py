9ce40490450833cb60873479480e6286b2be4f87
rendong <rzhang6@uvm.edu>
2019-09-01 20:35:46 -0400

    pygame window up tp step 77

--- Del01.py
+++ Del01.py
@@ -1,6 +1,13 @@
-import random
+import sys
 
+sys.path.insert(0, '..')
+import Leap
+from Leap import Finger
+from Leap import Bone
+import random
 from pygameWindow import PYGAME_WINDOW
+
+
 
 
 x = 450
@@ -20,8 +27,36 @@ def Perturb_Circle_Position():
     elif fourSidedDieRoll == 4 :
         y = y + 1
 
+def Handle_Frame(frame):
+    global x,y
+    hand = frame.hands[0]
+    print(hand)
+
+    fingers = hand.fingers
+
+    indexFingerList = hand.fingers.finger_type(Finger.TYPE_INDEX)
+    indexFinger = indexFingerList[0]
+    print(indexFinger)
+    distalPhalanx = indexFinger.bone(Bone.TYPE_DISTAL)
+    tip = distalPhalanx.next_joint
+    x = int(tip[0])
+    y = int(tip[1])
+    
+    print(type(tip))
+    print(tip)
+    print(x)
+    print(y)
+
+controller = Leap.Controller()
 while True:
     pygameWindow.Prepare()
+    frame = controller.frame()
+    handlist = frame.hands
+
+    for hand in handlist:
+        if (len(str(hand)) > 0):
+            Handle_Frame(frame)
+
     pygameWindow.Draw_Black_Circle(x,y)
     Perturb_Circle_Position()
     pygameWindow.Reveal()
